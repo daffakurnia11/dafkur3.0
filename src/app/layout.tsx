c0b4656b8a676d1202cdb1eb082ba4d22c744852
Daffa Kurnia Fatah <daffa@itsavirus.com>
2023-11-10 15:13:30 +0800

    build: :building_construction: Import icons

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,9 +1,7 @@
 import type { Metadata } from "next";
-import { Inter } from "next/font/google";
 import "./globals.css";
 import "@/styles/app.scss";
-
-const inter = Inter({ subsets: ["latin"] });
+import "@/icons/style.css";
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -17,7 +15,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body>{children}</body>
     </html>
   );
 }
