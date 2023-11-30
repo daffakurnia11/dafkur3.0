5c3ac274a53a8a2c314ad9ece7e15acb2dc81935
Daffa Kurnia Fatah <daffa@itsavirus.com>
2023-12-01 07:52:35 +0800

    feat: :sparkles: Create box project card component

--- src/app/project/page.tsx
+++ src/app/project/page.tsx
@@ -1,35 +1,11 @@
 "use client";
 
-import React, { HTMLAttributes, useMemo } from "react";
+import React from "react";
 import { Container } from "react-bootstrap";
 import { lgLayout, mdLayout, smLayout } from "./projectLayout";
 import GridLayout from "./GridLayout";
 import { motion } from "framer-motion";
-import Icon from "@/components/Icon";
-import Image from "next/image";
-
-interface Props extends HTMLAttributes<HTMLDivElement> {
-  shape: "vertical" | "horizontal" | "box";
-}
-
-const ProjectCard = (props: Props) => {
-  const shapeClass = useMemo(() => {
-    switch (props.shape) {
-      case "vertical":
-        return "card-vertical";
-      case "horizontal":
-        return "card-horizontal";
-      default:
-        return "card-box";
-    }
-  }, [props.shape]);
-
-  return (
-    <div className={`${shapeClass} project-card`}>
-      <div className="project-card-content p-4">{props.children}</div>
-    </div>
-  );
-};
+import { ProjectCard } from "@/components/Card";
 
 export default function Project() {
   const layouts = { lg: lgLayout, md: lgLayout, sm: mdLayout, xs: smLayout };
@@ -44,100 +20,40 @@ export default function Project() {
         >
           <GridLayout layouts={layouts}>
             <div key={`horizontal1`}>
-              <ProjectCard shape="horizontal">
-                <div className="position-absolute bottom-0 start-0 mb-4 ms-4">
-                  <div className="project-link d-flex justify-content-center align-items-center mb-3">
-                    <Icon
-                      icon="arrow-top-right"
-                      className="social-arrow"
-                      size={16}
-                    />
-                  </div>
-                  <h2 className="text-heading-5 text-light-green">
-                    Smart Hydroponic
-                  </h2>
-                  <p className="text-body text-light-green mt-2">
-                    Lorem ipsum dolor sit amet.
-                  </p>
-                  <div className="d-flex gap-2 mt-3">
-                    <span className="project-techstack d-block text-small text-light-green">
-                      Laravel
-                    </span>
-                    <span className="project-techstack d-block text-small text-light-green">
-                      Bootstrap5
-                    </span>
-                  </div>
-                </div>
-                <div className="position-absolute project-image">
-                  <Image
-                    src={"/project/smarthydroponic.png"}
-                    alt="Project Image"
-                    width={485}
-                    height={273}
-                  />
-                </div>
-              </ProjectCard>
-            </div>
-            <div key={`horizontal2`}>
-              <ProjectCard shape="horizontal">horizontal 2</ProjectCard>
+              <ProjectCard
+                shape="horizontal"
+                name="Smart Hydroponic"
+                description="Lorem ipsum dolor sit amet."
+                tech={["Laravel", "Bootstrap5"]}
+                image="smarthydroponic.png"
+              />
             </div>
             <div key={`vertical1`}>
-              <ProjectCard shape="vertical">
-                <div className="project-content">
-                  <div className="position-absolute bottom-0 start-0 mb-4 ms-4">
-                    <div className="project-link d-flex justify-content-center align-items-center mb-3">
-                      <Icon
-                        icon="arrow-top-right"
-                        className="social-arrow"
-                        size={16}
-                      />
-                    </div>
-                    <h2 className="text-heading-5 text-light-green">
-                      IFEM 2022
-                    </h2>
-                    <p className="text-body text-light-green mt-2">
-                      Lorem ipsum dolor sit amet.
-                    </p>
-                    <div className="d-flex gap-2 mt-3">
-                      <span className="project-techstack d-block text-small text-light-green">
-                        Laravel
-                      </span>
-                      <span className="project-techstack d-block text-small text-light-green">
-                        Bootstrap5
-                      </span>
-                    </div>
-                  </div>
-                  <div className="position-absolute project-image">
-                    <Image
-                      src={"/project/ifem.png"}
-                      alt="Project Image"
-                      width={180}
-                      height={360}
-                    />
-                    <Image
-                      src={"/project/ifem.png"}
-                      alt="Project Image"
-                      width={180}
-                      height={360}
-                    />
-                  </div>
-                </div>
-              </ProjectCard>
-            </div>
-            <div key={`vertical2`}>
-              <ProjectCard shape="vertical">vertical 2</ProjectCard>
+              <ProjectCard
+                shape="vertical"
+                name="IFEM 2022"
+                description="Lorem ipsum dolor sit amet."
+                tech={["Laravel", "Bootstrap5"]}
+                image="ifem.png"
+              />
             </div>
             <div key={`box1`}>
-              <ProjectCard shape="box">Box 1</ProjectCard>
+              <ProjectCard
+                shape="box"
+                name="EPW 2022"
+                description="Lorem ipsum dolor sit amet."
+                image="epw.png"
+                background="#84AB4F"
+              />
             </div>
             <div key={`box2`}>
-              <ProjectCard shape="box">Box 2</ProjectCard>
-            </div>
-            <div key={`box3`}>
-              <ProjectCard shape="box">Box 3</ProjectCard>
-            </div>
-            <div key={`box4`}>
-              <ProjectCard shape="box">Box 4</ProjectCard>
+              <ProjectCard
+                shape="box"
+                name="Breakpoint"
+                description="Lorem ipsum dolor sit amet."
+                image="epw.png"
+                background="#0D1726"
+              />
             </div>
           </GridLayout>
         </motion.div>
