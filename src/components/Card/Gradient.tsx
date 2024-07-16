a4b2cbe258b23961c42e41439e274afff9cd71a3
Daffa Kurnia Fatah <daffa@itsavirus.com>
2024-07-17 06:52:33 +0700

    refactor: :fire: Remove unused codes

--- src/components/Card/Gradient.tsx
+++ src/components/Card/Gradient.tsx
@@ -14,14 +14,14 @@ const GradientCard = ({
   <div
     {...props}
     className={classNames(
-      "p-[1px] bg-gradient-to-b from-green-primary from-30% to-green-primary/15 hover:bg-green-primary to-80% rounded-[18px] transition duration-300",
+      "p-[1px] bg-gradient-to-b from-green-primary from-30% to-green-primary/15 hover:bg-green-primary to-80% rounded-[18px] transition-all duration-300",
       isSquare && "aspect-square",
       className
     )}
   >
     <div
       className={classNames(
-        "w-full bg-black rounded-[17px] p-5 sm:p-8 relative",
+        "w-full h-full bg-black rounded-[17px] p-5 sm:p-8 relative",
         isSquare && "aspect-square"
       )}
     >
--- src/components/Card/index.ts
+++ src/components/Card/index.ts
@@ -1,9 +1,7 @@
 import GradientCard from "./Gradient";
 import SocialCard from "./Social";
 
-export * from "./GradientCard";
 export * from "./ProjectCard";
-export * from "./CardContact";
 
 const Card = {
   Gradient: GradientCard,
