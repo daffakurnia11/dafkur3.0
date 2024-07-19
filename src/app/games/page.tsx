a30d994296e670fcfa629728edb33c26789b832f
Daffa Kurnia Fatah <daffa@itsavirus.com>
2024-07-19 16:04:10 +0700

    feat: :sparkles: Add Games page

--- src/app/games/page.tsx
+++ src/app/games/page.tsx
@@ -1,12 +1,12 @@
 import React from "react";
 import type { Metadata } from "next";
-import AboutPage from "./AboutPage";
+import GamesPage from "./GamesPage";
 
 export const metadata: Metadata = {
   title: "Coming Soon - Dafkur 3.0.0",
   description: "Welcome to Dafkur 3.0.0",
 };
 
-export default function About() {
-  return <AboutPage />;
+export default function Games() {
+  return <GamesPage />;
 }
